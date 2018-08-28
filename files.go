284c45511fa652b3d523d7abbc478251c2b6634e
akiel <akiel@aleph.engineering>
2018-08-28 17:32:08 -0400

    Add comments to exported functions

--- files.go
+++ files.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// GetFileSize gets the size of a file
 func GetFileSize(afile string) int64 {
 	fi, err := os.Stat(afile)
 	if err != nil {
@@ -14,6 +15,7 @@ func GetFileSize(afile string) int64 {
 	return fi.Size()
 }
 
+//FileExists checks if a file exists
 func FileExists(afile string) bool {
 	if _, err := os.Stat(afile); os.IsNotExist(err) {
 		return false
